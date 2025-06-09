f6d93c82f03b5aa04721c744065008e1a726ad03
TuanPham <tuanpntse173039@fpt.edu.vn>
2025-06-09 07:54:09 +0700

    add global store

--- src/app/(landing)/my-tests/page.tsx
+++ src/app/(landing)/my-tests/page.tsx
@@ -1,4 +1,5 @@
 'use client';
+import useBearStore from '@/stores/useBearStore';
 import Button from '@mui/material/Button';
 import { useRouter } from 'next/navigation';
 
@@ -7,13 +8,18 @@ const TestLandingPage = () => {
   const routing = useRouter()
 
   const handleClick = () => {
-    routing.push('/my-courses');
+    routing.push('/my-courses/12332');
   }
+
+  const bearStore = useBearStore();
 
   return (
     <div>
-      <h1>Test Landing Page</h1>
+      <h1>Test Landing Page - Bear - {bearStore.bears}</h1>
       <p>Welcome to the test landing page. Here you can find various tests and quizzes.</p>
+
+      <Button variant="contained" onClick={() => bearStore.increase(2)}>Increase Bear</Button>
+
       <Button variant="contained" onClick={handleClick}>My course</Button>
     </div>
   )
---
+++ src/stores/useBearStore.ts
@@ -0,0 +1,16 @@
+import { create } from 'zustand'
+
+
+interface BearState {
+  bears: number
+  increase: (by: number) => void
+  decrease: (by: number) => void
+}
+
+const useBearStore = create<BearState>()((set) => ({
+  bears: 0,
+  increase: (by) => set((state) => ({ bears: state.bears + by })),
+  decrease: (by) => set((state) => ({ bears: state.bears - by })),
+}))
+
+export default useBearStore
