606e5c45c8985b8a0714e1909f790a389248437b
doanhung <hungdmse173190@fpt.edu.vn>
2025-06-19 22:21:37 +0700

    chore: add Zustand as a dependency and remove pnpm lock file

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -15,7 +15,6 @@ export const metadata: Metadata = {
 };
 
 export default function Ecommerce() {
-  const bearStore = useBearStore();
   return (
     <div className="grid grid-cols-12 gap-4 md:gap-6">
       <div className="col-span-12 space-y-6 xl:col-span-7">
